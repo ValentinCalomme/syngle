8479ad2277dcaf03f5f3634bc0cf3b04951a2360
valentincalomme <info@valentincalomme.com>
2020-09-29 09:58:43 +0200

    :hammer: add bin/deploy.sh script

---
+++ bin/deploy.sh
@@ -0,0 +1,14 @@
+# This script pushes code to master, updates the docs website and pushes
+# to pypi
+
+if git diff-index --quiet master --; then
+
+    git push origin master
+
+    python -m mkdocs gh-deploy
+
+    poetry publish --build
+
+else
+    echo "Please commit all changes before committing."
+fi
