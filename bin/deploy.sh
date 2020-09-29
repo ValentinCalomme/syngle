d6fa7be7fa9d5d2b2b05cc3b12b005ac0d50dad9
valentincalomme <info@valentincalomme.com>
2020-09-29 10:11:02 +0200

    :bug: fix deploy script to only run if branch is up-to-date

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,14 +3,21 @@
 
 sh bin/build.sh
 
-if git diff-index --quiet master --; then
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
 
-    git push origin master
+if [ $LOCAL = $REMOTE ]; then
 
     python -m mkdocs gh-deploy
 
-    poetry publish --build
+    poetry publish
 
+elif [ $LOCAL = $BASE ]; then
+    echo "The local branch is behind the remote."
+elif [ $REMOTE = $BASE ]; then
+    echo "The local branch is ahead of the remote, please push first."
 else
-    echo "Please commit all changes before committing."
+    echo "The local and remote branch have diverged, please fix that first."
 fi
