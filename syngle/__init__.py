60d03806efeb904998d7a1a9c92f15c2176dcbf6
valentincalomme <info@valentincalomme.com>
2020-09-30 14:45:26 +0200

    :recycle: fix code style

--- syngle/__init__.py
+++ syngle/__init__.py
@@ -1,9 +1,8 @@
 """Improve convenience of imports."""
 
-__version__ = "0.2.0"
-
-
 from syngle.decorator import singleton
 from syngle.metaclass import Singleton
+
+__version__ = "0.2.0"
 
 __all__ = ["Singleton", "singleton"]
